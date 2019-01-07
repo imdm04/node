8a9aae98336dae70d26ea80350e6745bd55d1ba8
DM <darshanmakadia506@gmail.com>
2019-01-07 21:33:55 +0530

    7 complete

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+
+const getProductsFromFIle = cb => {
+
+    fs.readFile(p, (err, fileContent) => {
+        if(err)
+        {
+           cb([]);
+        }
+        else
+        {
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
+module.exports = class Product{
+    constructor(t){
+        this.title = t;
+    }
+
+    save() {
+        const p = path.join(path.dirname(process.mainModule.filename),'data','products.json');
+        
+        getProductsFromFIle( products =>{
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+        //fs.readFile(p, (err, fileContent) => {});
+    }
+
+    static fetchAll(cb){
+        getProductsFromFIle(cb);
+    }
+}
+
