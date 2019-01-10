6704a4987819d6fac7a66749e8f28a17234bf52c
imdm04 <darshanmakadia506@gmail.com>
2019-01-10 22:51:40 +0530

    193 complete

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
     .save()
     .then(result => {
@@ -62,7 +69,6 @@ exports.postEditProduct = (req, res, next) => {
     updatedImageUrl,
     prodId
   );
-
   product
     .save()
     .then(result => {
