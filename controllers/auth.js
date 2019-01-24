72a4f129d8db490d73cbf7592376996088cfda99
imdm04 <darshanmakadia506@gmail.com>
2019-01-24 19:14:56 +0530

    complete 16

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridtransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridtransport({
+  auth:{
+    api_key: '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'
+  }
+})); 
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -82,6 +90,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
